8b65a7ce761502bb3b61fc493e10b2f41802407b
YS <yungsoon9807@gmail.com>
2022-11-14 19:10:00 +0800

    remove search

--- boundary/admin_searchUserProfilePage.php
+++ boundary/admin_searchUserProfilePage.php
@@ -143,7 +143,7 @@
                             <a href="admin_searchUserPage.php" class="nav-link">
                                 <i class="nav-icon"><img src="../img/search.svg"></i>
                                 <p class="navHeader">
-                                    Search user
+                                View user
                                 </p>
                             </a>
                         </li>
@@ -151,7 +151,7 @@
                             <a href="admin_searchUserProfilePage.php" class="nav-link">
                                 <i class="nav-icon"><img src="../img/search.svg"></i>
                                 <p class="navHeader">
-                                    Search user profile
+                                View user profile
                                 </p>
                             </a>
                         </li>
