6d9e357a4492b6064869e72dc90503ee5872e27f
KX <68456598+Kexin97@users.noreply.github.com>
2022-11-16 03:14:33 +0800

    author

---
+++ controller/viewRatingController.php
@@ -0,0 +1,29 @@
+<?php
+    require_once("../entity/accountEntity.php");
+
+    class ViewRatingController
+    {
+        protected $entity;
+        protected $result;
+
+		
+        function __construct()
+        {
+            $this->entity = new Paper();
+            $this->result = array();
+        }
+
+		public function reviewRatingList($paperName)
+        {
+            $this->result = $this->entity->reviewRating($paperName);
+
+            return $this->result;
+        }
+		
+		
+    }
+	
+	
+	
+	
+?>
